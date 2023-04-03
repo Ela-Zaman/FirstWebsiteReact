a529fe78a8e72e28075b382e91efbd6724ed3f48
Ela Zaman <raziazaman@iut-dhaka.edu>
2023-04-03 11:41:40 +0600

    implemented header and brand component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import {Footer,Blog, Possibility,Features,WhatGPT3,Header} from './containers';
-import {CTA,Brand,Navbar} from './components';
+import {CTA,Brand,Brand2,Navbar} from './components';
 
 const App = () => {
   return (
---
+++ src/components/brand2/Brand2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./brand2.css"
+const Brand2 = () => {
+  return (
+    <div>
+      Brand2
+    </div>
+  )
+}
+
+export default Brand2

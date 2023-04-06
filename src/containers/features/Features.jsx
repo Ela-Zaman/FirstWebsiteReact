a55014ef989a857d46529e9d9c3c5ab5253b43da
Ela Zaman <raziazaman@iut-dhaka.edu>
2023-04-06 12:15:27 +0600

    implement whatgpt3

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './features.css';
 const Features = () => {
   return (
-    <div>
+    <div className='gpt3__features section__padding' id="features">
       Features
     </div>
   )
